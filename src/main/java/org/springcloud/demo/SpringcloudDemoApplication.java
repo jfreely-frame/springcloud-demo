f0c8e1fa2178af3f6bdc8a1df370e81a9083380c
qjx <393657562@qq.com>
2018-11-20 14:38:09 +0800

    springcloud-demo新建

---
+++ src/main/java/org/springcloud/demo/SpringcloudDemoApplication.java
@@ -0,0 +1,12 @@
+package org.springcloud.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudDemoApplication.class, args);
+	}
+}
